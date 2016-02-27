e43ab36e928e9e13f01d4977bd4f8a29de401cf6
Angus McLeod <angus@mcleod.org.au>
2016-02-26 21:43:11 -0500

    example of making the solution to a solved topic the topic preview

--- plugin.rb
+++ plugin.rb
@@ -4,3 +4,24 @@
 # authors: Angus McLeod
 
 register_asset 'stylesheets/previews.scss', :desktop
+
+after_initialize do
+
+  require 'listable_topic_serializer'
+  class ::ListableTopicSerializer
+
+    def excerpt
+      accepted_id = object.custom_fields["accepted_answer_post_id"].to_i
+      if accepted_id > 0
+        cooked = Post.where(id: accepted_id).pluck('cooked')
+        PrettyText.excerpt(cooked[0], 200, {})
+      else
+        object.excerpt
+      end
+    end
+
+    def include_excerpt?
+      true
+    end
+  end
+end
