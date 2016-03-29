37c678dff098b5693af21baa83a28507f4aea652
Angus McLeod <angus@mcleod.org.au>
2016-03-29 07:43:54 -0400

    Fix custom fields && add list_excerpt field

--- plugin.rb
+++ plugin.rb
@@ -7,6 +7,9 @@ register_asset 'stylesheets/previews.scss'
 
 after_initialize do
 
+  Category.register_custom_field_type('list_thumbnails', :boolean)
+  Category.register_custom_field_type('list_excerpts', :boolean)
+
   require 'listable_topic_serializer'
   class ::ListableTopicSerializer
 
@@ -15,7 +18,7 @@ after_initialize do
 
       if accepted_id > 0
         cooked = Post.where(id: accepted_id).pluck('cooked')
-        excerpt = PrettyText.excerpt(cooked[0], 200, {})
+        excerpt = PrettyText.excerpt(cooked[0], 300, {})
       else
         excerpt = object.excerpt
       end
@@ -29,9 +32,32 @@ after_initialize do
 
   end
 
-  TopicList.preloaded_custom_fields << "accepted_answer_post_id" if TopicList.respond_to? :preloaded_custom_fields
+  require 'topic_list_item_serializer'
+  class ::TopicListItemSerializer
+    attributes :show_thumbnail, :show_excerpt
 
-  add_to_serializer(:topic_list_item, :show_thumbnail) {object.category.custom_fields["list_thumbnails"] && !!object.image_url}
-  add_to_serializer(:basic_category, :preview_enabled) {object.custom_fields["list_thumbnails"] == 'true'}
+    def show_thumbnail
+      object.category.custom_fields["list_thumbnails"] && !!object.image_url
+    end
+
+    def show_excerpt
+      object.category.custom_fields["list_excerpts"] && !!object.excerpt
+    end
+  end
+
+  require 'basic_category_serializer'
+  class ::BasicCategorySerializer
+    attributes :list_thumbnails, :list_excerpts
+
+    def list_thumbnails
+      object.custom_fields["list_thumbnails"]
+    end
+
+    def list_excerpts
+      object.custom_fields["list_excerpts"]
+    end
+  end
+
+  TopicList.preloaded_custom_fields << "accepted_answer_post_id" if TopicList.respond_to? :preloaded_custom_fields
   add_to_serializer(:suggested_topic, :is_suggested) {true}
 end
