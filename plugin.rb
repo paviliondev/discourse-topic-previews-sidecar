76a011dc5d4dc3cee95ae676bfb2cc88bbe717f9
Angus McLeod <angus@mcleod.org.au>
2016-04-10 15:30:48 -0400

    Save thumbnails in topic custom fields after initial post creation

--- plugin.rb
+++ plugin.rb
@@ -9,6 +9,7 @@ after_initialize do
 
   Category.register_custom_field_type('list_thumbnails', :boolean)
   Category.register_custom_field_type('list_excerpts', :boolean)
+  Topic.register_custom_field_type('thumbnails', :json)
 
   require 'listable_topic_serializer'
   class ::ListableTopicSerializer
@@ -27,21 +28,69 @@ after_initialize do
 
   end
 
+  module ListHelper
+    class << self
+      def create_thumbnails(image)
+        return nil if !image
+        normal = thumbnail_url(image, 100, 100)
+        retina = thumbnail_url(image, 200, 200)
+        { normal: normal, retina: retina }
+      end
+
+      def thumbnail_url (image, w, h)
+        image.create_thumbnail!(w, h) if !image.has_thumbnail?(w, h)
+        image.thumbnail(w, h).url
+      end
+    end
+  end
+
+  require 'cooked_post_processor'
+  class ::CookedPostProcessor
+
+    def get_linked_image(url)
+      max_size = SiteSetting.max_image_size_kb.kilobytes
+      file = FileHelper.download(url, max_size, "discourse", true) rescue nil
+      file ? Upload.create_for(@post.user_id, file, file.path.split('/')[-1], File.size(file.path)) : nil
+    end
+
+    def create_topic_thumbnails(url)
+      local = UrlHelper.is_local(url)
+      image = local ? (Upload.get_from_url(url) rescue nil) : get_linked_image(url)
+      thumbnails = ListHelper.create_thumbnails(image)
+      topic = Topic.find(@post.topic.id)
+      topic.custom_fields['thumbnails'] = thumbnails
+      topic.save_custom_fields
+    end
+
+    def update_topic_image
+      if @post.is_first_post?
+        img = extract_images_for_topic.first
+        return if !img["src"]
+        url = img["src"][0...255]
+        @post.topic.update_column(:image_url, url)
+        create_topic_thumbnails(url)
+      end
+    end
+
+  end
+
   require 'topic_list_item_serializer'
   class ::TopicListItemSerializer
     attributes :show_thumbnail, :thumbnails
 
-    def thumbnail_url(image, w, h)
-      image.create_thumbnail!(w, h) if !image.has_thumbnail?(w, h)
-      image.thumbnail(w, h).url
+    def get_thumbnails_from_image_url
+      image = Upload.get_from_url(object.image_url) rescue false
+      ListHelper.create_thumbnails(image)
     end
 
     def thumbnails
-      image = Upload.get_from_url(object.image_url)
-      return false if !image
-      normal = thumbnail_url(image, 100, 100)
-      retina = thumbnail_url(image, 200, 200)
-      { normal: normal, retina: retina }
+      return false unless object.archetype == Archetype.default
+      if object.custom_fields['thumbnails']
+        object.custom_fields['thumbnails']
+      else
+        ## for backup and legacy support
+        object.image_url ? get_thumbnails_from_image_url : false
+      end
     end
 
     def show_thumbnail
@@ -67,5 +116,6 @@ after_initialize do
   end
 
   TopicList.preloaded_custom_fields << "accepted_answer_post_id" if TopicList.respond_to? :preloaded_custom_fields
+  TopicList.preloaded_custom_fields << "thumbnails" if TopicList.respond_to? :preloaded_custom_fields
   add_to_serializer(:suggested_topic, :is_suggested) {true}
 end
