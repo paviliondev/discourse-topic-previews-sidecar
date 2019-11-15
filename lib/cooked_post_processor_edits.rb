d2bdde786506acd4de9ccfde8bd4ef2dc68a1022
Robert Barrow <merefield@gmail.com>
2019-11-15 13:03:20 +0000

    ensure thumbnails are included in tracked changes for post revisor

--- lib/cooked_post_processor_edits.rb
+++ lib/cooked_post_processor_edits.rb
@@ -26,68 +26,58 @@ CookedPostProcessor.class_eval do
 
   def update_post_image
 
-    unless @post.topic.custom_fields['user_thumbnail_selected']
+    img = extract_post_image
+    
+    if @has_oneboxes
+      cooked = PrettyText.cook(@post.raw)
 
-      return unless @post.topic.custom_fields['thumbnail_from_post'].blank? || @post.topic.custom_fields['thumbnail_from_post'] == @post.id
+      if img
+        ## We need something more specific to identify the image with
+        img_id = img
+        src = img.attribute("src").to_s
+        img_id = src.split('/').last.split('.').first if src
+      end
 
-      @post.topic.custom_fields['user_thumbnail_selected'] = false
-
-      img = extract_post_image
-
-      if @has_oneboxes
-        cooked = PrettyText.cook(@post.raw)
-
-        if img
-          ## We need something more specific to identify the image with
-          img_id = img
-          src = img.attribute("src").to_s
-          img_id = src.split('/').last.split('.').first if src
-        end
-
-        prior_oneboxes = []
-        Oneboxer.each_onebox_link(cooked) do |url, element|
-          if !img || (img && cooked.index(element).to_i < cooked.index(img_id).to_i)
-            html = Nokogiri::HTML::fragment(Oneboxer.cached_preview(url))
-            prior_oneboxes = html.css('img')
-          end
-        end
-
-        if prior_oneboxes.any?
-          prior_oneboxes = prior_oneboxes.reject do |html|
-            class_str = html.attribute('class').to_s
-            class_str.include?('site-icon') || class_str.include?('avatar')
-          end
-
-          if prior_oneboxes.any? && validate_image_for_previews(prior_oneboxes.first)
-            img = prior_oneboxes.first
-          end
+      prior_oneboxes = []
+      Oneboxer.each_onebox_link(cooked) do |url, element|
+        if !img || (img && cooked.index(element).to_i < cooked.index(img_id).to_i)
+          html = Nokogiri::HTML::fragment(Oneboxer.cached_preview(url))
+          prior_oneboxes = html.css('img')
         end
       end
 
-      if img.blank?
-        @post.update_column(:image_url, nil)
-
-        if @post.is_first_post?
-          @post.topic.update_column(:image_url, nil)
-          ListHelper.remove_topic_thumbnails(@post.topic)
+      if prior_oneboxes.any?
+        prior_oneboxes = prior_oneboxes.reject do |html|
+          class_str = html.attribute('class').to_s
+          class_str.include?('site-icon') || class_str.include?('avatar')
         end
-      elsif img["src"].present?
-        url = img["src"][0...255]
-        @post.update_column(:image_url, url) # post
 
-        if @post.is_first_post?
-          @post.topic.update_column(:image_url, url) # topic
-          return if SiteSetting.topic_list_hotlink_thumbnails ||
-                    !SiteSetting.topic_list_previews_enabled
-
-          if upload_id = ListHelper.create_topic_thumbnails(@post, url)
-            # make a record of the upload id so it can be included in the PostUpload update later in the process
-            @post.custom_fields['thumbnail_upload_id'] = upload_id
-          end
+        if prior_oneboxes.any? && validate_image_for_previews(prior_oneboxes.first)
+          img = prior_oneboxes.first
         end
       end
-    else
-      @post.topic.custom_fields['user_thumbnail_selected'] = false
+    end
+    
+    if img.blank?
+      @post.update_column(:image_url, nil)
+      if @post.is_first_post? && (@post.topic.custom_fields['thumbnail_from_post'].blank? || @post.topic.custom_fields['thumbnail_from_post'] == @post.id)
+        @post.topic.update_column(:image_url, nil)
+        ListHelper.remove_topic_thumbnails(@post.topic)
+      end
+    elsif img["src"].present?
+      url = img["src"][0...255]
+      @post.update_column(:image_url, url) # post
+
+      if @post.is_first_post? && @post.topic.custom_fields['thumbnail_from_post'].blank?
+        @post.topic.update_column(:image_url, url) # topic
+        return if SiteSetting.topic_list_hotlink_thumbnails ||
+                  !SiteSetting.topic_list_previews_enabled
+
+        if upload_id = ListHelper.create_topic_thumbnails(@post, url)[:id]
+          # make a record of the upload id so it can be included in the PostUpload update later in the process
+          @post.custom_fields['thumbnail_upload_id'] = upload_id
+        end
+      end
     end
   end
 end
--- lib/topic_list_previews_helper.rb
+++ lib/topic_list_previews_helper.rb
@@ -9,9 +9,9 @@ module ListHelper
       local = UrlHelper.is_local(url)
       image = local ? Upload.find_by(sha1: url[/[a-z0-9]{40,}/i]) : get_linked_image(post, url)
       Rails.logger.info "Creating thumbnails with: #{image}"
-      create_thumbnails(post.topic, image, url)
+      thumbnails = create_thumbnails(post.topic, image, url)
       post.image_url = url
-      image.id
+      return {id: image.id, thumbnails: thumbnails}
     end
 
     def get_linked_image(post, url)
