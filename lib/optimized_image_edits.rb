1aa23d9ec3fc194c4315b448f7f0165dc88cf021
Robert Barrow <merefield@gmail.com>
2022-08-05 17:15:41 +0100

    modify other image processing methods

--- lib/optimized_image_edits.rb
+++ lib/optimized_image_edits.rb
@@ -43,4 +43,67 @@ OptimizedImage.class_eval do
       #{to}
     })
   end
+
+  def self.crop_instructions(from, to, dimensions, opts = {})
+    ensure_safe_paths!(from, to)
+
+    from = prepend_decoder!(from, to, opts)
+    to = prepend_decoder!(to, to, opts)
+
+    instructions = ['convert', "#{from}[0]"]
+
+    if SiteSetting.topic_list_enable_thumbnail_black_border_elimination
+      instructions.concat(%W{
+        -fuzz 4%
+        -define trim:percent-background=0%
+        -trim
+        +repage
+      })
+    end
+
+    instructions.concat(%W{
+      -auto-orient
+      -gravity north
+      -background transparent
+      -#{thumbnail_or_resize} #{dimensions}^
+      -crop #{dimensions}+0+0
+      -unsharp 2x0.5+0.7+0
+      -interlace none
+      -profile #{File.join(Rails.root, 'vendor', 'data', 'RT_sRGB.icm')}
+    })
+
+    if opts[:quality]
+      instructions << "-quality" << opts[:quality].to_s
+    end
+
+    instructions << to
+end
+
+  def self.downsize_instructions(from, to, dimensions, opts = {})
+    ensure_safe_paths!(from, to)
+
+    from = prepend_decoder!(from, to, opts)
+    to = prepend_decoder!(to, to, opts)
+
+    instructions = ['convert', "#{from}[0]"]
+
+    if SiteSetting.topic_list_enable_thumbnail_black_border_elimination
+      instructions.concat(%W{
+        -fuzz 4%
+        -define trim:percent-background=0%
+        -trim
+        +repage
+      })
+    end
+
+    instructions.concat(%W{
+      -auto-orient
+      -gravity center
+      -background transparent
+      -interlace none
+      -resize #{dimensions}
+      -profile #{File.join(Rails.root, 'vendor', 'data', 'RT_sRGB.icm')}
+      #{to}
+    })
+  end
 end
