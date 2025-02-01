658ab0d44613a964e060353b17b7bccd5f208aa5
merefield <merefield@gmail.com>
2025-02-01 11:56:34 +0000

    FIX: expose Topic Colour Determination setting to client

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-topic-previews-sidecar
 # about: Sidecar Plugin to support Topic List Preview Theme Component
-# version: 6.0.0
+# version: 6.0.1
 # authors: Robert Barrow, Angus McLeod
 # url: https://github.com/paviliondev/discourse-topic-previews
 
